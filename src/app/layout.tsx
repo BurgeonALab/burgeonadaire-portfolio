244556a7d0b3e015fe9dbd1e46c5bb8a1afc6696
Gerald Zandisko <geraldzandisko@burgeonadaire.com>
2024-11-27 17:21:51 +0700

    hosting init

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,28 @@
 import type { Metadata } from "next";
 import { Red_Hat_Text } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 const inter = Red_Hat_Text({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Histories | Burgeon Adaire Portfolio",
-  description: "Burgeon Adaire Portfolio.",
+  title: "Portfolio | PT. Burgeon Adaire International",
+  description: "Burgeon Adaire Portfolios.",
   authors: [
     {
       name: "PT. Burgeon Adaire International",
       url: "https://burgeonadaire.com"
     }
   ],
+  openGraph: {
+    title: "Portfolio | PT. Burgeon Adaire International",
+    description: "Burgeon Adaire Portfolios.",
+    locale: "en_US",
+    alternateLocale: "id_ID",
+    type: "website",
+    url: "https://portfolio.burgeonadaire.com",
+    siteName: "Burgeon Adaire Portfolio",
+  }
 };
 
 export default function RootLayout({
@@ -27,6 +37,17 @@ export default function RootLayout({
       >
         {children}
       </body>
+      
+      <Script id="gtg-script1" async src="https://www.googletagmanager.com/gtag/js?id=G-E739TR23LQ"></Script>
+      <Script id="gtg-script2">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-E739TR23LQ');
+        `}
+      </Script>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,38 @@
 import {
   Fragment,
 } from "react";
+import Link from 'next/link';
+import Head from 'next/head';
+
+const jsonLd = {
+  '@context': 'https://schema.org',
+  '@type': 'WebSite',
+  'name': 'Burgeon Adaire Portfolio',
+  'alternateName': 'BAP',
+  'url': 'https://portfolio.burgeonadaire.com'
+}
 
 export default function Home() {
   return (
-    <Fragment></Fragment>
+    <Fragment>
+      <Head>
+        <link
+          rel="canonical"
+          href="https://portfolio.burgeonadaire.com"
+          key="canonical"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="follow" />
+      </Head>
+      <div className="flex flex-col justify-center items-center h-screen">
+        <p className="font-medium text-lg sm:text-2xl">Burgeon Adaire <span className="font-normal">Portfolio</span></p>
+
+        <Link className='blog-link' href="https://burgeonadaire.com" target='_blank' rel='noopener'>
+          <p className="mt-3 font-normal">Coming Soon</p>
+        </Link>
+      </div>
+
+      <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }} />
+    </Fragment>
   );
 }
